f5e64f6e4f24cfb95fde05b6bf109d672109d10c
Sajed Azizi <sajed@sajeds-air.lan>
2021-08-26 16:43:20 +0100

    Updates

---
+++ precache-manifest.48fb7d172d3c82e375af3f1c8a5d7029.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a3aa96fe49289a0b7d282305e3c629",
+    "url": "/punk-api/index.html"
+  },
+  {
+    "revision": "4e42801bdb10ca536302",
+    "url": "/punk-api/static/css/main.4f01d102.chunk.css"
+  },
+  {
+    "revision": "31d1e9038c0167ee3071",
+    "url": "/punk-api/static/js/2.030379b7.chunk.js"
+  },
+  {
+    "revision": "55b79ebb5e774a9f9242c9bbc135e8e0",
+    "url": "/punk-api/static/js/2.030379b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e42801bdb10ca536302",
+    "url": "/punk-api/static/js/main.248f7119.chunk.js"
+  },
+  {
+    "revision": "73be6eeae2d4f59e76ea",
+    "url": "/punk-api/static/js/runtime-main.b0afb95a.js"
+  },
+  {
+    "revision": "8484de1b4d4b39f79353fa4d5dd3e01b",
+    "url": "/punk-api/static/media/brewdog.8484de1b.jpeg"
+  }
+]);
